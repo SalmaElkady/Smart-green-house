113dbfc8cc4172b72d6e7e37d0ed242ae8ca5020
MOHAMED <abo657920@gmail.com>
2026-04-20 15:54:46 +0200

    Update grblUpload_copy_20260420115244.ino

--- grblUpload_copy_20260420115244.ino
+++ grblUpload_copy_20260420115244.ino
@@ -6,11 +6,8 @@
 #define SOILPIN 7
 #define PUMPPIN 2
 
-DHT dht(DHTPIN, DHTTYPE); // عملني object لي الحسلس عشان يطلع قرا 
+DHT dht(DHTPIN, DHTTYPE); 
 
-// شغلت Serial و sensor 
-// و خليت التربه input و pump output  
-// وقافلت pump 
 
 
 void setup() {
@@ -19,7 +16,7 @@ void setup() {
   pinMode(SOILPIN, INPUT);
   pinMode(PUMPPIN, OUTPUT);
   
-  // تأكد إنها مقفولة في البداية
+
   digitalWrite(PUMPPIN, HIGH); 
   delay(1000);
   Serial.println("Ready to Upload!");
@@ -29,14 +26,13 @@ void loop() {
   float t = dht.readTemperature();
   int soilStatus = digitalRead(SOILPIN);
 
- // بيجيب درجه الحراره 
- // و بيشوف التربه ناشفه ولا مبلوله 
+
 
 
   Serial.print("Temp: "); Serial.print(t);
   Serial.print("C | Soil: "); 
 
-  // بيطبع درجه الحراره و حاله التربه 
+
 
   if (soilStatus == HIGH) { 
     Serial.println("DRY -> PUMP ON");
@@ -46,6 +42,6 @@ void loop() {
     Serial.println("WET -> PUMP OFF");
     digitalWrite(PUMPPIN, HIGH); 
   }
- // بنعمل if لو التربه ناشفه يزود مايه لو مبلوله يقفل  pump 
+
   delay(1000); 
 }
